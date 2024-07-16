b4cd255da98c9f7699434986f2b1d8fc966cec8d
KassioKebraTD <k.felippe11@gmail.com>
2024-07-15 22:04:25 -0300

    first commit

---
+++ src/test/java/com/kassio_kebra_td/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kassio_kebra_td.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
